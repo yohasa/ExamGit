f5d4970466ee6bdeb7f231a32f56e6ea4fbcc5d8
yhs <yhsaghrounu@gmail.com>
2023-06-18 10:36:36 +0300

    v.2

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -12,7 +12,7 @@ internal class Program
     {
         static void Main(string[] args)
         {
-                Console.WriteLine("Hello World");
+                Console.WriteLine("bye bye World");
          
 
         }
